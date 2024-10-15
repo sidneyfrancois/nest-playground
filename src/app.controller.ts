daec8f6b2dd3019228ffde14b2b3ba7300e08e9a
Sidney François <sidney.fvm.ps@outlook.com>
2024-10-14 21:04:05 -0300

    feat: add simple SSE to notify client for 5 seconds

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { Observable } from 'rxjs';
+
+interface MessageEvent {
+  data: string | object;
+  id?: string;
+  type?: string;
+  retry?: number;
+}
 
 @Controller()
 export class AppController {
@@ -16,5 +24,29 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('status')
+  @Sse('status-stream')
+  getStatus(): Observable<MessageEvent> {
+    let count = 0;
+    return new Observable((observer) => {
+      const interval = setInterval(() => {
+        observer.next({
+          data: { status: `Uploading ${count++}` },
+          type: 'notification',
+        });
+      }, 1000);
+
+      const timeout = setTimeout(() => {
+        clearInterval(interval);
+        observer.complete();
+      }, 5000);
+
+      return () => {
+        clearInterval(interval);
+        clearTimeout(timeout);
+      };
+    });
   }
 }
